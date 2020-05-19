fa131c11cb5955a965e5eeb7aeb9845fdb866db8
Marerina <marivasi96@gmail.com>
2020-05-19 13:38:38 +0500

    Первичная загрузка

---
+++ HistoryManager/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryManager
+{
+    public class Character
+    {
+        public int ProjectID { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Info { get; set; }
+        public string Biography { get; set; }
+        public string Ability { get; set; }
+        public string Group { get; set; }
+    }
+
+    public   struct Field
+    {
+        public string Name { get; set; }
+        public string Vaue { get; set; }
+    }
+}
